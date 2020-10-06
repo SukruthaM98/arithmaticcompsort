148d5132a7db3107ff8099030f82c2c402f851d4
SukruthaM98 <msukrutha@gmail.com>
2020-10-06 16:23:59 +0530

    input from user have been taken

--- ArithmeticComputations.sh
+++ ArithmeticComputations.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 
 echo " welcome to arithmetic computation program"
+#taking inputs
+read -p "enter three values/numbers for computation" a b c
+echo " numbers entered by user are:" $a $b $c
