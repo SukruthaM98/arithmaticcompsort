2ecc6d8fa64bee6ab150db1aae9013df6916e5bd
SukruthaM98 <msukrutha@gmail.com>
2020-10-06 16:26:56 +0530

    first computation made

--- ArithmeticComputations.sh
+++ ArithmeticComputations.sh
@@ -4,3 +4,8 @@ echo " welcome to arithmetic computation program"
 #taking inputs
 read -p "enter three values/numbers for computation" a b c
 echo " numbers entered by user are:" $a $b $c
+
+#first computation
+
+computation1=$(( $a+$b*$c ))
+echo "first computation is:" $computation1
